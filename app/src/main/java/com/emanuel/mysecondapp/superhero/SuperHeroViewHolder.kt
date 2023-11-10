ce269fc37fac7333e9c143ebb3e58f72e3606978
emanuelhardwell <emanuelnoe16@gmail.com>
2023-11-09 22:46:27 -0600

    feat: The SuperHeroViewHolder was created

---
+++ app/src/main/java/com/emanuel/mysecondapp/superhero/SuperHeroViewHolder.kt
@@ -0,0 +1,17 @@
+package com.emanuel.mysecondapp.superhero
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.emanuel.mysecondapp.databinding.ItemSuperheroBinding
+import com.squareup.picasso.Picasso
+
+class SuperHeroViewHolder (view: View): RecyclerView.ViewHolder(view) {
+
+    private var binding= ItemSuperheroBinding.bind(view)
+
+     fun bind(superHero: SuperHeroItem) {
+         binding.textViewSuperHeroName.text= superHero.name
+         //binding.imageViewSuperHero
+         Picasso.get().load(superHero.image.url).into(binding.imageViewSuperHero);
+    }
+}
