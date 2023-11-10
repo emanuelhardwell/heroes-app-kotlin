6b20cb9def0840bb9786d754ef58603ec339a4d8
emanuelhardwell <emanuelnoe16@gmail.com>
2023-11-09 23:04:00 -0600

    feat: The data class SuperHeroImage and SuperHeroItem was created

--- app/src/main/java/com/emanuel/mysecondapp/superhero/SuperHeroDataResponse.kt
+++ app/src/main/java/com/emanuel/mysecondapp/superhero/SuperHeroDataResponse.kt
@@ -2,5 +2,18 @@ package com.emanuel.mysecondapp.superhero
 
 import com.google.gson.annotations.SerializedName
 
-data class SuperHeroDataResponse(@SerializedName("response") val response: String) {
+data class SuperHeroDataResponse(
+    @SerializedName("response") val response: String,
+    @SerializedName("results") val results: List<SuperHeroItem>
+) {
 }
+
+data class SuperHeroItem(
+    @SerializedName("id") val id: String,
+    @SerializedName("name") val name: String,
+    @SerializedName("image") val image: SuperHeroImage
+)
+
+data class SuperHeroImage(
+    @SerializedName("url") val url: String
+)
