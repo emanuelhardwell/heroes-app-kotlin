8e91dedef2ca258bd386899a7866c220c67dc049
emanuelhardwell <emanuelnoe16@gmail.com>
2023-11-09 22:44:37 -0600

    feat: The SuperHeroAdapter was created

---
+++ app/src/main/java/com/emanuel/mysecondapp/superhero/SuperHeroAdapter.kt
@@ -0,0 +1,28 @@
+package com.emanuel.mysecondapp.superhero
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.emanuel.mysecondapp.R
+
+class SuperHeroAdapter(var superHeroItemList: List<SuperHeroItem> = emptyList()) :
+    RecyclerView.Adapter<SuperHeroViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SuperHeroViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        return SuperHeroViewHolder(layoutInflater.inflate(R.layout.item_superhero, parent, false))
+    }
+
+    override fun onBindViewHolder(viewHolder: SuperHeroViewHolder, position: Int) {
+        val superHero = superHeroItemList[position]
+        return viewHolder.bind(superHero)
+    }
+
+    override fun getItemCount(): Int {
+        return superHeroItemList.size
+    }
+
+    fun updateSuperHeroItemList(superHeroItemList: List<SuperHeroItem> ){
+        this.superHeroItemList = superHeroItemList
+        notifyDataSetChanged()
+    }
+}
