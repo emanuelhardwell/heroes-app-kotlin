a3cacb22b3d133e4ec08d77fcaed0e3ec97f6a64
emanuelhardwell <emanuelnoe16@gmail.com>
2023-11-10 23:02:54 -0600

    feat: The SuperHeroDetailResponse was created

---
+++ app/src/main/java/com/emanuel/mysecondapp/superhero/SuperHeroDetailResponse.kt
@@ -0,0 +1,24 @@
+package com.emanuel.mysecondapp.superhero
+
+import com.google.gson.annotations.SerializedName
+
+data class SuperHeroDetailResponse(
+    @SerializedName("response") var response: String,
+    @SerializedName("id") var id: String,
+    @SerializedName("name") var name: String,
+    @SerializedName("powerstats") var powerstats: SuperHeroPowerstats,
+    @SerializedName("image") val image: SuperHeroDetailImage
+)
+
+data class SuperHeroDetailImage(
+    @SerializedName("url") val url: String
+)
+
+data class SuperHeroPowerstats(
+    @SerializedName("intelligence") val intelligence: String,
+    @SerializedName("strength") val strength: String,
+    @SerializedName("speed") val speed: String,
+    @SerializedName("durability") val durability: String,
+    @SerializedName("power") val power: String,
+    @SerializedName("combat") val combat: String,
+)
