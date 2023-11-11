ed4b515b157b2e90658bd1411f971381e74f1d17
emanuelhardwell <emanuelnoe16@gmail.com>
2023-11-11 00:04:01 -0600

    feat: The SuperHeroAdapter was updated

--- app/src/main/java/com/emanuel/mysecondapp/superhero/SuperHeroAdapter.kt
+++ app/src/main/java/com/emanuel/mysecondapp/superhero/SuperHeroAdapter.kt
@@ -5,7 +5,7 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.emanuel.mysecondapp.R
 
-class SuperHeroAdapter(var superHeroItemList: List<SuperHeroItem> = emptyList()) :
+class SuperHeroAdapter(var superHeroItemList: List<SuperHeroItem> = emptyList(), var onSelectedItem: (String) -> Unit ) :
     RecyclerView.Adapter<SuperHeroViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SuperHeroViewHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
@@ -14,14 +14,14 @@ class SuperHeroAdapter(var superHeroItemList: List<SuperHeroItem> = emptyList())
 
     override fun onBindViewHolder(viewHolder: SuperHeroViewHolder, position: Int) {
         val superHero = superHeroItemList[position]
-        return viewHolder.bind(superHero)
+        return viewHolder.bind(superHero, onSelectedItem)
     }
 
     override fun getItemCount(): Int {
         return superHeroItemList.size
     }
 
-    fun updateSuperHeroItemList(superHeroItemList: List<SuperHeroItem> ){
+    fun updateSuperHeroItemList(superHeroItemList: List<SuperHeroItem>) {
         this.superHeroItemList = superHeroItemList
         notifyDataSetChanged()
     }
